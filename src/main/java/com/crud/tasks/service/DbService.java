e8fd17ffbdedd71fa51b9fb3b2abc27b98092f2f
“tomaz866 <twlodarski86@gmail.com>
2017-11-12 15:20:17 +0100

    Zadanie 15.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -18,5 +19,17 @@ public class DbService {
 
     public Task getTaskById(final Long id) {
         return repository.findById(id).orElse(null);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
     }
 }
