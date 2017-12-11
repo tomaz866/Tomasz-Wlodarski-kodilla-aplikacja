29bc53ea2b39b91cc91e6365431ce90ca243493b
“tomaz866 <twlodarski86@gmail.com>
2017-12-11 14:46:20 +0100

    Wersja 1.5

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,7 @@ public class EmailScheduler {
 
     private static final String SUBJECT = "Tasks: Once a day email";
 
-    @Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(cron = "0 0 * * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String t = " tasks.";
